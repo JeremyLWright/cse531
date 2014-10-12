ff166cd0fd9f98fbf49caa4da8fceb7ab4c92b2f
Matt Welch <mattw0308@gmail.com>
2014-10-11 17:50:29 -0700

    Added messaging for debugging odd threads behavior: not returning to original context.

--- thread_test.c
+++ thread_test.c
@@ -8,22 +8,36 @@ static const int sleep_usec = 250;
 
 // TODO: define more interesting functions that may be passed to start_thread()
 void function1(){
-    static int i = 10;
+    int i = 10;
+    static int numCalls = 0;
+    ++numCalls;
     while(1) {
         i++;
         usleep(sleep_usec);
-        printf("Function1 waking up for the %dth time - Global %d...\n", i, globalInt1);
+        if (numCalls%100 == 0) {
+        printf("Function1 (instance %d) waking up for the %dth time - Global %d...\n", 
+            numCalls, i, globalInt1);
+        }
         yield();
     }
 }
 
 void function2(){
-    static int i = 0;
+    int i = 0;
+    static int numCalls = 0;
+    ++numCalls;
     while(1) {
         i++;
         usleep(sleep_usec);
-        printf("Function2 is awake and computing (%d times) - Global %d...\n", i, globalInt2);
+        if (numCalls%100 == 0) {
+            printf("Function2 (instance %d) is awake and computing (%d times) - Global %d...\n", 
+                    numCalls, i, globalInt2);
+        }
         yield();
+        // it seems that we're never returning from the yield and spawning new threads instead
+        globalInt2++;
+        printf("Function2 (instance %d) is back in context (%d times) - Global %d...\n", 
+            numCalls, i, globalInt2);
     }
 }
 
@@ -32,19 +46,26 @@ int main(int argc, const char *argv[])
 {
 
     int i = 0;
+    int totalThreads = 250000;
+    if (argc > 1){
+        totalThreads = atoi(argv[1]);
+    }
+    printf("Begin Main function\n");
+    printf("Main function: spawning %d child threads...\n", totalThreads);
     // allocate a RunQ
     InitQ(&RunQ);
-    printf("Begin Main thread\n");
 
     // Let's see Linux Kernel try to manage 1M threads! :-P
     // Yeah, 13,105,680 KB of memory
-    for(i = 0; i < 1000000; ++i) 
+    for(i = 0; i < totalThreads; ++i) 
     {
-        start_thread(function1);    
+        start_thread(function1);
         start_thread(function2);
     }
+    printf("Main function: %d threads queued, ready to run:\n", i);
     run(); //I'm pretty sure this never returns
 
+    printf("Main function entering wait loop\n");
     while (1) //I'm pretty sure this never runs
     {
         usleep(sleep_usec);
