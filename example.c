f2043d11a5cd09d01b1e1b1a0607cd15602de4bb
Jeremy Wright <jeremy@codestrokes.com>
2014-10-12 12:14:24 -0700

    Working from new example code.

--- example.c
+++ example.c
@@ -2,55 +2,102 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static ucontext_t uctx_main, uctx_func1, uctx_func2;
+typedef struct _TCB_t {
+    ucontext_t ctx;
+} TCB_t;
+#define QUEUE_SIZE  3
+TCB_t ctx[QUEUE_SIZE];
+size_t idx = 0;
 
 #define handle_error(msg) \
     do { perror(msg); exit(EXIT_FAILURE); } while (0)
 
-static void
-func1(void)
+void yield()
 {
-    printf("func1: started\n");
-    printf("func1: swapcontext(&uctx_func1, &uctx_func2)\n");
-    if (swapcontext(&uctx_func1, &uctx_func2) == -1)
-        handle_error("swapcontext");
-    printf("func1: returning\n");
+    size_t current = idx;
+    idx = (idx + 1) % QUEUE_SIZE;
+    size_t next = idx;
+    printf("Status: (%lu, %lu)", current, next);
+    swapcontext(&ctx[current].ctx, &ctx[next].ctx);
+    printf("\tHello from yield.\n");
 }
 
-static void
+void
+func1(void)
+{
+    int i =0;
+    printf("func1: started\n");
+    while(1)
+    {
+        printf("[%d] ", ++i);
+    printf("func1: swapcontext(&uctx_func1, &uctx_func2)\n");
+    yield();
+    printf("func1: returning\n");
+    }
+}
+    
+void
 func2(void)
 {
+    int j = 10;
     printf("func2: started\n");
+    while(1)
+    {
+        printf("[%d] ", ++j);
     printf("func2: swapcontext(&uctx_func2, &uctx_func1)\n");
-    if (swapcontext(&uctx_func2, &uctx_func1) == -1)
-        handle_error("swapcontext");
+    yield();
     printf("func2: returning\n");
+    }
 }
+
+    void
+func3(void)
+{
+    int j = 10;
+    printf("func3: started\n");
+    while(1)
+    {
+        printf("[%d] ", ++j);
+    printf("func3: swapcontext(&uctx_func3, &uctx_func1)\n");
+    yield();
+    printf("func3: returning\n");
+    }
+}
+void start_thread(TCB_t* temp, void (*function)(void))
+{ // begin pseudo code
+    //allocate a stack (via malloc) of a certain size (choose 8192)
+    int const  stack_size = 8192;
+    getcontext(&temp->ctx);
+    temp->ctx.uc_stack.ss_sp = malloc(stack_size * sizeof(char) );
+    temp->ctx.uc_stack.ss_size = stack_size;
+    //temp->ctx.uc_link = &uctx_main;
+    makecontext(&temp->ctx, function, 0);
+}
+
+void run()
+{   // real code
+    ucontext_t parent;     // get a place to store the main context, for faking
+
+    //// int getcontext(ucontext_t *ucp); 
+    //// initializes the structure pointed at by ucp to the currently active context. 
+    getcontext(&parent);   // magic sauce
+
+    //swapcontext(&parent, RotateQ(&RunQ)->context);  // start the first thread
+    // swapcontext will return (0) only when the current context is reactivated
+   swapcontext(&parent, &ctx[0].ctx);
+}
+
 
 int
 main(int argc, char *argv[])
 {
-    char func1_stack[16384];
-    char func2_stack[16384];
+    start_thread(&ctx[0], func1);
+    start_thread(&ctx[1], func2);
+    start_thread(&ctx[2], func3);
 
-   if (getcontext(&uctx_func1) == -1)
-        handle_error("getcontext");
-    uctx_func1.uc_stack.ss_sp = func1_stack;
-    uctx_func1.uc_stack.ss_size = sizeof(func1_stack);
-    uctx_func1.uc_link = &uctx_main;
-    makecontext(&uctx_func1, func1, 0);
-
-   if (getcontext(&uctx_func2) == -1)
-        handle_error("getcontext");
-    uctx_func2.uc_stack.ss_sp = func2_stack;
-    uctx_func2.uc_stack.ss_size = sizeof(func2_stack);
-    /* Successor context is f1(), unless argc > 1 */
-    uctx_func2.uc_link = (argc > 1) ? NULL : &uctx_func1;
-    makecontext(&uctx_func2, func2, 0);
 
    printf("main: swapcontext(&uctx_main, &uctx_func2)\n");
-    if (swapcontext(&uctx_main, &uctx_func2) == -1)
-        handle_error("swapcontext");
+   run();
 
    printf("main: exiting\n");
     exit(EXIT_SUCCESS);
