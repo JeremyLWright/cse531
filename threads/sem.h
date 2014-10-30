dbb22c3b657f526b87d9aac1dccaa5f246556c16
Jeremy Wright <jeremy@codestrokes.com>
2014-10-29 19:57:56 -0700

    We're close. Who is waking up 4? If we can figure that you we can rotate and schedule writers 6 and 7. Right now those thrads block on sem3 and never wake up.

--- threads/sem.h
+++ threads/sem.h
@@ -1,9 +1,17 @@
 #pragma once
 #include "threads.h"
 
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
 #ifdef DEBUG
-#define S_TRACE() do{ printf("%s:%d Sem%lu=%d Thread: %lu\n", __func__, __LINE__, sem->sid, sem->count, sem->WaitQ.curr->tid); } while(0);
-#define T_TRACE() do{ printf("%s:%d Sem%lu=%d Thread: %lu\n", __func__, __LINE__, sem->sid, sem->count, RunQ.curr->tid); } while(0);
+#define S_TRACE() do{ printf(ANSI_COLOR_GREEN "%s:%d Sem%lu=%d Thread: %lu\n" ANSI_COLOR_RESET,  __func__, __LINE__, sem->sid, sem->count, sem->WaitQ.curr->tid); } while(0);
+#define T_TRACE() do{ printf(ANSI_COLOR_BLUE"%s:%d Sem%lu=%d Thread: %lu\n" ANSI_COLOR_RESET, __func__, __LINE__, sem->sid, sem->count, RunQ.curr->tid); } while(0);
 #else
 #define S_TRACE()
 #define T_TRACE()
@@ -28,11 +36,14 @@ void sem_yield(semaphore_t* sem)
 {
     T_TRACE();
     AddQ(&sem->WaitQ, DelQ(&RunQ));
+    S_TRACE();
     swapcontext(&sem->WaitQ.curr->ctx, &RunQ.curr->ctx);
+    RotateQ(&sem->WaitQ);
 }
 
 void P(semaphore_t* sem)
 {
+    T_TRACE();
     if(sem->count > 0)
     {
         --sem->count;
