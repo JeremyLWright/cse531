01fef1a6db4e02a5b9903805d1128267e86fe727
Matt Welch <mattw0308@gmail.com>
2014-11-12 22:15:11 -0700

    Added a consumer signal to msgs.h::Send and removed it from msgs.h::Receive because receivers (servers) would not know that a message was in the queue when it was sent then they would wake up each other when no message was available.  Added more output messaging to port2.test.c.

--- tests/port2.test.c
+++ tests/port2.test.c
@@ -8,43 +8,55 @@ typedef int message_value_type;
 
 Port_t portA;
 int payload1a[] = {1,2,3,4};
+int writer_id = 0;
+int reader_id = 0;
 
 void writer(void)
 {
+    int myID = ++writer_id;
+    printf("Writer thread [%d] beginning\n",myID);
     int j = 0;
     while(1)
     {
         message_t msg1a = make_message(payload1a, 4);
+        //printf("WRITER: payload1a[%d]=%d, msg1a[%d]=%d\n", 0, payload1a[0],0,msg1a.payload[0]);
         Send(&portA, msg1a);
         sleep(1);
-        printf("Writer Run: %d\n", ++j);
+        printf("Writer[%d] Run: %d\n", myID, ++j);
         printf("Successful Writes: %lu\n", portA.write_idx);
     }
 }
 
 void reader(void)
 {
+    int myID = ++reader_id;
+    printf("\tReader thread [%d] beginning\n", myID);
     int i, j = 0;
     while(1)
     {
         message_t msg1b = Receive(&portA);
-        for(i = 0; i < sizeof(payload1a)/sizeof(message_value_type); ++i)
+        for(i = 0; i < sizeof(payload1a)/sizeof(message_value_type); ++i){
+           // printf("\tREADER[%d]: payload1a[%d]=%d, msg1b[%d]=%d\n", 
+           //     myID, i, payload1a[i],i,msg1b.payload[i]);
             assert(payload1a[i] == msg1b.payload[i]);
+         }
         sleep(1);
-        printf("Reader Run: %d\n", ++j);
+        printf("\tReader[%d] Run: %d\n", myID, ++j);
     }
 }
 
 
 int main(int argc, const char *argv[])
 {
-    printf("This test should crash with a deadlock after 10 iterations.\n\
+    printf("If there are no available readers to clear the port queue,\n
+    this test will crash with a deadlock after 10 iterations.\n\
 There are no readers to pull from the queue, thus the writer \n\
 will fill up the queue, get pushed on the semaphore's queue, \n\
 then the process will exit since there are no runnable threads.\n");
 
     InitQ(&RunQ);
     PortInit(&portA, 10);
+    start_thread(writer);
     start_thread(writer);
     start_thread(writer);
     start_thread(writer);
--- threads/msgs.h
+++ threads/msgs.h
@@ -23,20 +23,20 @@ void PortInit(Port_t* portA, size_t max_size)
     portA->write_idx = 0;
     portA->read_idx = 0;
     portA->data = (message_t*)malloc(sizeof(message_t)*max_size);
-    portA->producer_sem = CreateSem(max_size-1);
-    portA->consumer_sem = CreateSem(1);
+    portA->producer_sem = CreateSem(max_size-1);// init to max_size - 1 since that many open spaces
+    portA->consumer_sem = CreateSem(0);// should init the consumer to 0 since initially nothing in the buffer
     portA->mutex = CreateSem(1);
 }
 
 message_t make_message(message_value_type* payload, size_t payload_size)
 {
-    message_t a;
+    message_t msg;
     
     assert(payload_size <= message_size);
 
-    a.payload_size = payload_size;
-    memcpy(a.payload, payload, payload_size*sizeof(message_value_type));
-    return a;
+    msg.payload_size = payload_size;
+    memcpy(msg.payload, payload, payload_size*sizeof(message_value_type));
+    return msg;
 }
 
 void Send(Port_t* port, const message_t msg)
@@ -45,28 +45,32 @@ void Send(Port_t* port, const message_t msg)
     {
         printf("\tProducer Sem: %d\n", port->producer_sem.count);
         P(&port->producer_sem);
-        memcpy(&port->data[port->write_idx], &msg, sizeof(msg));
-        port->write_idx++;
-        port->write_idx = port->write_idx % port->max_size;
-    }
+            memcpy(&port->data[port->write_idx], &msg, sizeof(msg));
+            port->write_idx++;
+            port->write_idx = port->write_idx % port->max_size;
+            V(&port->consumer_sem);
+    }//  the send should issue a signal here notifying the reader
+    // if no signal to the reader, he will never wake up on a successful receive
     //V(&port->mutex);
 }
 
 message_t Receive(Port_t* port)
 {
-    message_t m;
+    message_t msg;
     //P(&port->mutex);
     {
         P(&port->consumer_sem); //Block if we are the only reader
 
-        memcpy(&m, &port->data[port->read_idx], sizeof(message_t));
-        port->read_idx++;
-        port->read_idx = port->read_idx % port->max_size;
+            memcpy(&msg, &port->data[port->read_idx], sizeof(message_t));
+            port->read_idx++;
+            port->read_idx = port->read_idx % port->max_size;
 
-        V(&port->consumer_sem); //Unblock any waiting readers
-        V(&port->producer_sem); //Unblock any waiting producers to notify we read
-        printf("\t\tProducer Sem: %d\n", port->producer_sem.count);
+        //V(&port->consumer_sem); //Unblock any waiting readers 
+        // if the consumer unblocks waiting readers, they wil get double-unblocked by writer
+            V(&port->producer_sem); //Unblock any waiting producers to notify we read
+            printf("\t\tConsumer Sem: %d\n", port->consumer_sem.count);
+            printf("\t\tProducer Sem: %d\n", port->producer_sem.count);
     }
     //V(&port->mutex);
-    return m;
+    return msg;
 }
